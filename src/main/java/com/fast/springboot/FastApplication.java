0eaf8015c6764b4f013623f3dd5301e851bea18c
code-YuJun <2594131385@qq.com>
2026-04-13 13:29:49 +0800

    feat: 项目配置

---
+++ src/main/java/com/fast/springboot/FastApplication.java
@@ -0,0 +1,15 @@
+package com.fast.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+// 排除数据源的自动配置，因为需要自定义数据源的配置
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+public class FastApplication {
+    // 整个 java 程序的入口点
+    public static void main(String[] args) {
+        // 启动 spring boot 应用
+        SpringApplication.run(FastApplication.class, args);
+    }
+}
